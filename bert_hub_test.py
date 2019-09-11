d2739d3614c1856af7ac71929173494667320296
franklear <xy961218@gmail.com>
2019-09-11 16:37:04 +0800

    bert hub test

--- bert_hub_test.py
+++ bert_hub_test.py
@@ -14,12 +14,12 @@ BERT_MODEL_HUB = "bert/hub_bert_cased_L-12_H-768_A-12"
 
 def create_tokenizer_from_hub_module():
     """Get the vocab file and casing info from the Hub module."""
-    with tf.Graph().as_default():
-        bert_module = hub.Module(BERT_MODEL_HUB, trainable=True)
-        tokenization_info = bert_module(signature="tokenization_info", as_dict=True)
-        with tf.Session() as sess:
-            vocab_file, do_lower_case = sess.run([tokenization_info["vocab_file"],
-                                                  tokenization_info["do_lower_case"]])
+
+    bert_module = hub.Module(BERT_MODEL_HUB, trainable=True)
+    tokenization_info = bert_module(signature="tokenization_info", as_dict=True)
+    with tf.Session() as sess:
+        vocab_file, do_lower_case = sess.run([tokenization_info["vocab_file"],
+                                              tokenization_info["do_lower_case"]])
 
     return bert.tokenization.FullTokenizer(
         vocab_file=vocab_file, do_lower_case=do_lower_case)
@@ -27,6 +27,23 @@ def create_tokenizer_from_hub_module():
 
 tokenizer = create_tokenizer_from_hub_module()
 t = tokenizer.wordpiece_tokenizer.tokenize("This here's an example of using the BERT tokenizer")
+t = ['[CLS]'] + t + ['[SEP]']
 print(t)
 i = tokenizer.convert_tokens_to_ids(t)
 print(i)
+
+bert_module = hub.Module(BERT_MODEL_HUB, trainable=True)
+bert_inputs = dict(
+    input_ids=[i],
+    input_mask=[[0] * len(i)],
+    segment_ids=[[0] * len(i)]
+)
+bert_outputs = bert_module(
+    inputs=bert_inputs,
+    signature="tokens",
+    as_dict=True
+)
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    ret = sess.run(bert_outputs)
+print(ret)
