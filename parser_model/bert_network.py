dc48f4aa1154b79748ac839e71c5710ee0e6bcc1
longxud <longxud@hit.edu.cn>
2019-09-11 16:35:59 +0800

    add BERTNetwork config

--- parser_model/bert_network.py
+++ parser_model/bert_network.py
@@ -46,107 +46,49 @@ class BERTNetwork(BaseNetwork):
   #=============================================================
   def build_graph(self, input_network_outputs={}, reuse=True):
     """"""
-    
-    outputs = {}
     with tf.variable_scope('Embeddings'):
-      input_tensors = [input_vocab.get_input_tensor(reuse=reuse) for input_vocab in self.input_vocabs]
-      for input_network, output in input_network_outputs:
-        with tf.variable_scope(input_network.classname):
-          input_tensors.append(input_network.get_input_tensor(output, reuse=reuse))
-      layer = tf.concat(input_tensors, 2)
+        input_tensors = [input_vocab.get_input_tensor(reuse=reuse) for input_vocab in self.input_vocabs]
+
+        for input_network, output in input_network_outputs:
+            with tf.variable_scope(input_network.classname):
+                input_tensors.append(input_network.get_input_tensor(output, reuse=reuse))
+        layer = tf.concat(input_tensors, 2)
+
     n_nonzero = tf.to_float(tf.count_nonzero(layer, axis=-1, keep_dims=True))
     batch_size, bucket_size, input_size = nn.get_sizes(layer)
     layer *= input_size / (n_nonzero + tf.constant(1e-12))
-    
-    token_weights = nn.greater(self.id_vocab.placeholder, 0, dtype=tf.int32)
+
+    token_weights = nn.greater(self.id_vocab.placeholder, 0)
     tokens_per_sequence = tf.reduce_sum(token_weights, axis=1)
     n_tokens = tf.reduce_sum(tokens_per_sequence)
     n_sequences = tf.count_nonzero(tokens_per_sequence)
     seq_lengths = tokens_per_sequence + 1
+
+    root_weights = token_weights + (1 - nn.greater(tf.range(bucket_size), 0))
+    token_weights3D = tf.expand_dims(token_weights, axis=-1) * tf.expand_dims(root_weights, axis=-2)
     tokens = {'n_tokens': n_tokens,
               'tokens_per_sequence': tokens_per_sequence,
               'token_weights': token_weights,
+              'token_weights3D': token_weights,
               'n_sequences': n_sequences}
-    
+
     conv_keep_prob = 1. if reuse else self.conv_keep_prob
     recur_keep_prob = 1. if reuse else self.recur_keep_prob
     recur_include_prob = 1. if reuse else self.recur_include_prob
-    
-    rev_layer = tf.reverse_sequence(layer, seq_lengths, seq_axis=2)
-    for i in six.moves.range(self.n_layers):
-      conv_width = self.first_layer_conv_width if not i else self.conv_width
-      with tf.variable_scope('RNN_FW-{}'.format(i)):
-        layer, _ = recurrent.directed_RNN(layer, self.recur_size, seq_lengths,
-                                          bidirectional=False,
-                                          recur_cell=self.recur_cell,
-                                          conv_width=conv_width,
-                                          recur_func=self.recur_func,
-                                          conv_keep_prob=conv_keep_prob,
-                                          recur_include_prob=recur_include_prob,
-                                          recur_keep_prob=recur_keep_prob,
-                                          cifg=self.cifg,
-                                          highway=self.highway,
-                                          highway_func=self.highway_func)
-      if self.bidirectional:
-        with tf.variable_scope('RNN_BW-{}'.format(i)):
-          rev_layer, _ = recurrent.directed_RNN(rev_layer, self.recur_size, seq_lengths,
-                                                bidirectional=False,
-                                                recur_cell=self.recur_cell,
-                                                conv_width=conv_width,
-                                                recur_func=self.recur_func,
-                                                conv_keep_prob=conv_keep_prob,
-                                                recur_keep_prob=recur_keep_prob,
-                                                recur_include_prob=recur_include_prob,
-                                                cifg=self.cifg,
-                                                highway=self.highway,
-                                                highway_func=self.highway_func)
-    ones = tf.ones([batch_size, 1, 1])
-    with tf.variable_scope('RNN_FW-{}/RNN/Loop'.format(i), reuse=True):
-      fw_initial_state = tf.get_variable('Initial_state')
-      n_splits = fw_initial_state.get_shape().as_list()[-1] / self.recur_size
-      fw_initial_state = tf.split(fw_initial_state, int(n_splits), -1)[0]
-      start_token = ones * fw_initial_state
-      layer = tf.reverse_sequence(layer, seq_lengths, seq_axis=2)
-      layer = layer[:,1:]
-      layer = tf.reverse_sequence(layer, seq_lengths-1, seq_axis=2)
-      layer = tf.concat([start_token, layer], axis=1)
-    if self.bidirectional:
-      with tf.variable_scope('RNN_BW-{}/RNN/Loop'.format(i), reuse=True):
-        bw_initial_state = tf.get_variable('Initial_state')
-        n_splits = bw_initial_state.get_shape().as_list()[-1] / self.recur_size
-        bw_initial_state = tf.split(bw_initial_state, int(n_splits), -1)[0]
-        stop_token = ones * bw_initial_state
-        rev_layer = tf.concat([stop_token, layer], axis=1)
-        rev_layer = tf.reverse_sequence(rev_layer, seq_lengths+1, seq_axis=2)[:,1:]
-      if self.bilin:
-        layer = tf.concat([layer*rev_layer, layer, rev_layer], axis=2)
-      else:
-        layer = tf.concat([layer, rev_layer], axis=2)
+
     
     output_vocabs = {vocab.field: vocab for vocab in self.output_vocabs}
     outputs = {}
-    with tf.variable_scope('Classifiers'):
-      if 'form' in output_vocabs:
-        vocab = output_vocabs['form']
-        outputs[vocab.field] = vocab.get_sampled_linear_classifier(
-          layer, self.n_samples,
-          token_weights=token_weights,
-          reuse=reuse)
-        self._evals.add('form')
-      if 'upos' in output_vocabs:
-        vocab = output_vocabs['upos']
-        outputs[vocab.field] = vocab.get_linear_classifier(
-          layer,
-          token_weights=token_weights,
-          reuse=reuse)
-        self._evals.add('upos')
-      if 'xpos' in output_vocabs:
-        vocab = output_vocabs['xpos']
-        outputs[vocab.field] = vocab.get_linear_classifier(
-          layer,
-          token_weights=token_weights,
-          reuse=reuse)
-        self._evals.add('xpos')
+    # with tf.variable_scope('Classifiers'):
+    #   if 'FormTokenVocab' in output_vocabs:
+    #     vocab = output_vocabs['FormTokenVocab']
+    #     outputs[vocab.field] = vocab.get_sampled_linear_classifier(
+    #       layer, self.n_samples,
+    #       token_weights=token_weights,
+    #       reuse=reuse)
+    #     self._evals.add('form')
+    print(tokens,'\n',outputs)
+
     return outputs, tokens
   
   #=============================================================
